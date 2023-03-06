3c9f7cd054a2554d8a0977295c607d7746226d4c
mokyeah <mzmmzm2008@gmail.com>
2023-03-06 22:17:03 +0800

    i lub yi hong
    lol jk

--- src/App.jsx
+++ src/App.jsx
@@ -33,13 +33,17 @@ function App() {
       }
     }, []);
   return (
-    <div className='justify-center items-center flex w-full h-screen text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>
+    <div className=''>
+      <div className='justify-center items-center flex w-full h-screen text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 '>
       <div className=' w-1/3 h-1/3 rounded-md bg-black border-2 border-sky-500'>
-        <div className='justify-center font-bold flex text-2xl  '>GENDER REVEAL</div>
+        <div className='justify-center font-bold flex text-2xl p-3  '>GENDER REVEAL</div>
         <div className='text-2xl justify-center flex p-5'>  <button onClick={handleLuck}>REVEAL YOUR FUCKING GENDER</button></div>
         {isLucky ? <div className='text-4xl justify-center items-center  flex p-10'>MAN</div> : <div className=' text-4xl justify-center items-center flex p-10'>GAY</div>}
         <div className='justify-center items-center flex '>Times you tried: {numTries}, such a loser </div>
+        {numTries > 10 && <div className='justify-center items-center flex text-3xl font-bold font-mono p-5'>I lub Yi Hong</div>}
 
+      </div>
+      
       </div>
       
     </div>
